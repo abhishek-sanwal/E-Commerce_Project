8f3bef2b099a98bd4d590a7bc497e94f00be4e65
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-11-12 11:52:01 +0530

    :sparkles: Refractored code to add app router and updated css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Collections from "./pages/Collections";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
+import Orders from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] ">
+      <Navbar />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
