4770bdff0555b483da9ce9e54b3fc0a41785b93a
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-11-12 17:13:31 +0530

    :sparkles: Mounted BestSeller Component in homepae

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import BestSeller from "../components/BestSeller";
 import Hero from "../components/Hero";
+import LatestCollections from "../components/LatestCollections";
 
 function Home() {
   return (
     <div>
       <Hero />
+      <LatestCollections />
+      <BestSeller />
     </div>
   );
 }
