3badf1abff9f7869be2da36ecdf749e6ca66c4f6
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-11-12 17:12:54 +0530

    :sparkles: Wrapped context around App to provide global contetx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import "./app.css";
 
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ShopContextProvider } from "./contexts/ShopContextProvider.jsx";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </BrowserRouter>
 );
